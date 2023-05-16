5adc9fd988ab6258641ebc34f149c04f14770b63
Gabriel Larramendi <larramendiga@gmail.com>
2023-05-16 10:24:53 -0300

    User Registration Form

--- src/main/java/com/larramendi/springboot/controllers/AuthController.java
+++ src/main/java/com/larramendi/springboot/controllers/AuthController.java
@@ -1,6 +1,8 @@
 package com.larramendi.springboot.controllers;
 
+import com.larramendi.springboot.dto.UserDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 //Classe que captura a requisicao e retorna uma view
@@ -11,5 +13,12 @@ public class AuthController {
     @GetMapping("/index")
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model){
+        UserDTO user = new UserDTO();
+        model.addAttribute("user", user);
+        return "register";
     }
 }
